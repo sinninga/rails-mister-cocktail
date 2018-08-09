7d5202241d6b856ee99398e7f9d330ee6c440a0d
Adam Sinning <asinning25@gmail.com>
2018-08-09 18:15:25 -0300

    doses controller done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -18,11 +18,12 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
+    @dose.destroy
   end
 
   private
 
   def dose_params
-      params.require(:dose).permit(:description, :cocktail_id)
+      params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
